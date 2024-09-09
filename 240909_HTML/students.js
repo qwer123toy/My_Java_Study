0e6d6ad8e0c4364deef4c6e3b257216c49515457
qwer123toy <39717105+qwer123toy@users.noreply.github.com>
2024-09-09 19:30:27 +0900

    240909_HTML
    Parm, get, 버튼액션, dialog 등

---
+++ 240909_HTML/students.js
@@ -0,0 +1,21 @@
+
+const data = [
+    {
+      name: "길동",
+      kor: 100,
+      eng: 100,
+      math: 100,
+    },
+    {
+      name: "둘리",
+      kor: 90,
+      eng: 90,
+      math: 90,
+    },
+    {
+      name: "도우너",
+      kor: 70,
+      eng: 70,
+      math: 80,
+    },
+  ];
